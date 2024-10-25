c5e58582ac3ddb51d11d2e3e09166ae5f4b40caf
Dimitar85898 <83722607+Dimitar85898@users.noreply.github.com>
2024-10-25 09:42:37 +0300

    [kprint] printing for 'd' and 'i'

--- kernel/src/kprint.c
+++ kernel/src/kprint.c
@@ -1,7 +1,9 @@
 #include <kprint.h>
 #include <serial.h>
+#include <string.h>
 void kprint_va(const char* fmt, va_list args) {
      char c;
+     char ibuf[20];
      while(*fmt) {
         switch(c=*fmt++) {
         case '%': {
@@ -15,6 +17,10 @@ void kprint_va(const char* fmt, va_list args) {
             case 's':
                 write_serial(va_arg(args, const char*));
                 break;
+            case 'd':
+            case 'i':
+                ibuf[itoa(ibuf, sizeof(ibuf)-1, va_arg(args, int))] = '\0';
+                goto write_ibuf;
             default:
                 write_serial("<Unknown fmt `");
                 write_serial_char(c);
@@ -25,5 +31,8 @@ void kprint_va(const char* fmt, va_list args) {
             write_serial_char(c);
             break;
         }
+        continue;
+write_ibuf:
+        write_serial(ibuf);
      }
 }
