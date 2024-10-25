6c51f21276e42df69ca429020553a3a8ec121e59
Dimitar85898 <83722607+Dimitar85898@users.noreply.github.com>
2024-10-25 09:49:24 +0300

    [kprint] printflike

--- kernel/include/kprint.h
+++ kernel/include/kprint.h
@@ -1,6 +1,7 @@
 #pragma once
 #include <stdarg.h>
 void kprint_va(const char* fmt, va_list args);
+static void kprint(const char* fmt, ...)  __attribute__((format(printf,1,2)));
 static void kprint(const char* fmt, ...) {
      va_list args;
      va_start(args, fmt);
