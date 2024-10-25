72fc795be59e7fa95523300edfc4326e7324588e
Dimitar85898 <83722607+Dimitar85898@users.noreply.github.com>
2024-10-25 09:47:45 +0300

    [kprint] %zu formatter

--- kernel/src/kprint.c
+++ kernel/src/kprint.c
@@ -21,6 +21,20 @@ void kprint_va(const char* fmt, va_list args) {
             case 'i':
                 ibuf[itoa(ibuf, sizeof(ibuf)-1, va_arg(args, int))] = '\0';
                 goto write_ibuf;
+            case 'z':
+                switch(c=*fmt++) {
+                case '\0':
+                    return;
+                case 'u':
+                    ibuf[sztoa(ibuf, sizeof(ibuf)-1, va_arg(args, size_t))] = '\0';
+                    goto write_ibuf;
+                default:
+                    write_serial("<Unknown fmt `z");
+                    write_serial_char(c);
+                    write_serial("`>");
+                    break;
+                }
+                break;
             default:
                 write_serial("<Unknown fmt `");
                 write_serial_char(c);
