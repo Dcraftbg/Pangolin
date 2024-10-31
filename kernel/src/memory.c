08a48348755805be9af5a9c71aec8b28aa8d184c
DcraftBg <czl_obshta@abv.bg>
2024-10-31 12:34:09 +0200

    [memory] Implement missing functions

--- kernel/src/memory.c
+++ kernel/src/memory.c
@@ -2,7 +2,31 @@
 #include <kernel.h>
 #include <bootutils.h>
 #include <kprint.h>
+#include <mem/plist.h>
 
+paddr_t kernel_alloc_phys_page() {
+    void* addr = plist_allocate_page(&kernel.plist);
+    if(addr) addr-=kernel.hhdm;
+    return (paddr_t)addr;
+}
+void  kernel_dealloc_phys_page(paddr_t page) {
+    plist_dealloc_page(&kernel.plist, ((void*)page)+kernel.hhdm);
+}
+paddr_t kernel_alloc_phys_pages(size_t pages) {
+    void* addr = plist_allocate_cont_pages(&kernel.plist, pages);
+    if(addr) addr-=kernel.hhdm;
+    return (paddr_t)addr;
+}
+void  kernel_dealloc_phys_pages(paddr_t start, size_t pages) {
+    plist_dealloc_cont_pages(&kernel.plist, ((void*)start)+kernel.hhdm, pages);
+}
+
+void* kernel_alloc_pages(size_t pages) {
+    return plist_allocate_cont_pages(&kernel.plist, pages);
+}
+void kernel_dealloc_pages(void* addr, size_t pages) {
+    plist_dealloc_cont_pages(&kernel.plist, addr, pages);
+}
 void init_mem() {
     kernel.hhdm = boot_get_hhdm();
     list_init(&kernel.plist.list);
