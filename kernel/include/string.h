f637f13cacd584fc108c6cc7124a782c68ab1d30
DcraftBg <czl_obshta@abv.bg>
2024-10-31 23:03:41 +0200

    [string] Extra functions to string.h

--- kernel/include/string.h
+++ kernel/include/string.h
@@ -3,8 +3,15 @@
 #include <stdint.h>
 
 size_t strlen(const char *str);
-
+void *memset (void *dest, int x          , size_t n);
+void *memcpy (void *dest, const void *src, size_t n);
+void *memmove(void* dest, const void* src, size_t n);
+int memcmp(const void* s1, const void* s2, size_t max);
+int strncmp(const char* s1, const char* s2, size_t max);
+int strcmp(const char *restrict s1, const char *restrict s2);
+size_t strlen(const char* cstr);
 const char* strflip(char* str, size_t len);
+
 size_t itoa(char* buf, size_t cap, int value);
 size_t sztoa(char* buf, size_t cap, size_t value);
 size_t utoha(char* buf, size_t cap, unsigned int value);
