cf41ab0a1080e015adb31e0baabbf29ce6e3651b
DcraftBg <czl_obshta@abv.bg>
2024-10-31 21:08:58 +0200

    [idt|gdt] Align IDT and TODO comments

--- kernel/include/kernel.h
+++ kernel/include/kernel.h
@@ -6,10 +6,16 @@
 typedef struct {
     PList     plist;
     uintptr_t hhdm;
+    // TODO: Remove this entirely.
+    // You can just place it on the stack.
+    // Its not necessary
     GDTR      gdtr;
     uint64_t  GDT[5];
+    // TODO: Remove this entirely.
+    // You can just place it on the stack.
+    // Its not necessary
     IDTR      idtr;
-    IDTEntry  idt[256];
+    IDTEntry  idt[256] __attribute__((aligned(4096)));
 } Kernel;
 
 extern Kernel kernel;
