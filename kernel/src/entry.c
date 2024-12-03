240f48e01275644b49dd200a2fda9bbdfd7d9c81
UnmappedStack <jake.steinburger0@gmail.com>
2024-12-04 07:58:57 +1100

    Add stack smashing handler

--- kernel/src/entry.c
+++ kernel/src/entry.c
@@ -6,6 +6,22 @@
 #include <cpu/exception.h>
 #include <mem/page.h>
 
+#if UINT32_MAX == UINTPTR_MAX
+    #define STACK_CHK_GUARD 0xe2dee396
+#else
+    #define STACK_CHK_GUARD 0x595e9fbd94fda766
+#endif
+
+uintptr_t __stack_chk_guard = STACK_CHK_GUARD;
+
+__attribute__((noreturn))
+void __stack_chk_fail(void) {
+	kprint("Stack smashing detected. Halting.\n");
+    asm volatile("cli");
+    for (;;)
+        asm volatile("hlt");
+}
+
 void _start() {
     init_serial();
     init_mem();
