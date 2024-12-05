f0a253d9fbd7ea24ed1f4c935b4da91c2ddf54bf
UnmappedStack <jake.steinburger0@gmail.com>
2024-12-05 22:01:09 +1100

    [slab] cache should not be fixed size

--- kernel/src/mem/slab.c
+++ kernel/src/mem/slab.c
@@ -6,8 +6,6 @@
 #include <memory.h>
 #include <string.h>
 
-/* A function to initialise a new slab cache.
- * TODO: Calculate obj_per_slab instead of taking it as an argument. */
 Cache *init_slab_cache(size_t obj_size, char *name) {
     char name_buf[20];
     strcpy(name_buf, name);
@@ -18,73 +16,58 @@ Cache *init_slab_cache(size_t obj_size, char *name) {
     }
     
     size_t obj_per_slab = (page_align_up(obj_size + sizeof(Slab) + sizeof(uint64_t)) - sizeof(Slab)) / (obj_size + sizeof(uint64_t));
-    size_t num_obj = obj_per_slab;
 
-    kprint("Initialising new slab cache: \"%s\" of object size %zu with %zu objects, with %zu objects per slab.\n",
-            name_buf, obj_size, num_obj, obj_per_slab);
-    
-    size_t stack_size = num_obj * sizeof(uint64_t);
-    size_t num_slabs = 1;
-    size_t cache_size = sizeof(Cache) + // size added from the cache
-                        sizeof(Slab) * num_slabs + // size added from the slabs metadata
-                        obj_size * num_obj + // size added from the actual data
-                        stack_size; // size added from the free stack.
-    Cache *new_cache = (Cache*) (kernel_alloc_phys_pages(bytes_to_pages(page_align_up(cache_size))) + kernel.hhdm);
-    kprint("New cache addr: %p, with %zu slabs and %zu byte cache size.\n", new_cache, num_slabs, cache_size);
+    Cache *new_cache = (Cache*) (kernel_alloc_phys_pages(bytes_to_pages(page_align_up(sizeof(Cache)))) + kernel.hhdm);
     
     *new_cache = (Cache) {
         .obj_size     = obj_size,
         .obj_per_slab = obj_per_slab,
     };
+    
     strcpy(new_cache->name, name_buf);
 
-    if (!kernel.slab_caches) {
-        kprint("Slab object list is empty, must initiate list.\n");
+    if (!kernel.slab_caches)
         list_init(&new_cache->list);
-    } else {
+    else
         list_insert(&new_cache->list, kernel.slab_caches);
-    }
     
     struct list *slab_lists[] = {&new_cache->free_nodes,
                                  &new_cache->partial_nodes,
                                  &new_cache->full_nodes};
     for (size_t slab_list = 0; slab_list < 3; slab_list++) list_init(slab_lists[slab_list]);
     
-    size_t slab_size = sizeof(Slab) + // slab size itself
-                       obj_size * obj_per_slab + // size of the data
-                       stack_size; // size of the stack
-                       
-    
-    for (size_t slab = 0; slab < num_slabs; slab++) {
-        Slab *new_slab = (Slab*) ((size_t) new_cache + sizeof(Cache) + (slab_size * slab));
-        void* data_addr = (void*) ((uint64_t) new_slab + stack_size);
-        *new_slab = (Slab) {
-            .cache = new_cache,
-            .free_stack_head = 0,
-            .num_free = obj_per_slab,
-            .data = data_addr,
-        };
-        list_insert(&new_slab->list, &new_cache->free_nodes);
-        
-        // this loop is a little messy, sorry. 
-        size_t data_element = 0;
-        for (size_t stack_element = (uint64_t) new_slab + sizeof(Slab); stack_element < (uint64_t) data_addr; stack_element += 8) {
-            *((uint64_t*) stack_element) = (uint64_t) data_addr + data_element;
-            data_element += obj_size;
-        }
-    }
-    
     return new_cache;
 }
 
-void *slab_alloc(Cache* cache) {
+void slab_grow(Cache *cache) {
+    Slab *new_slab = (Slab*) ((uint64_t) kernel_alloc_phys_pages(bytes_to_pages(page_align_up(sizeof(Slab) + cache->obj_size * cache->obj_per_slab))) + kernel.hhdm);
+    uint64_t stack_size = cache->obj_per_slab * sizeof(uint64_t);
+    void *data_addr = (void*) ((uint64_t) new_slab + stack_size);
+    *new_slab = (Slab) {
+        .cache = cache,
+        .free_stack_head = 0,
+        .num_free = cache->obj_per_slab,
+        .data = data_addr,
+    };
+    list_insert(&new_slab->list, &cache->free_nodes);
+    // this loop is a little messy, sorry. 
+    size_t data_element = 0;
+    for (size_t stack_element = (uint64_t) new_slab + sizeof(Slab); stack_element < (uint64_t) data_addr; stack_element += 8) {
+        *((uint64_t*) stack_element) = (uint64_t) data_addr + data_element;
+        data_element += cache->obj_size;
+    }
+}
+
+void *slab_alloc(Cache *cache) {
     Slab *slab = NULL;
     if (list_len(&cache->partial_nodes)) {
         slab = (Slab*) list_next(&cache->partial_nodes);
     } else if (list_len(&cache->free_nodes)) {
         slab = (Slab*) list_next(&cache->free_nodes);
-    } else
-        return NULL;
+    } else {
+        slab_grow(cache);
+        slab = (Slab*) list_next(&cache->free_nodes);
+    }
     uint64_t *free_stack = (uint64_t*) ((uint64_t) slab + sizeof(Slab));
     void *free_stack_element = (void*) free_stack[slab->free_stack_head];
     slab->free_stack_head++;
@@ -98,13 +81,3 @@ void *slab_alloc(Cache* cache) {
     }
     return free_stack_element;
 }
-
-
-
-
-
-
-
-
-
-
