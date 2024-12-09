56db799adb0cff30d1c0ee86f4327c1344c6d77c
DcraftBg <czl_obshta@abv.bg>
2024-12-09 10:42:30 +0200

    [entry] (TEMPORARY) File system test case

--- kernel/src/entry.c
+++ kernel/src/entry.c
@@ -10,6 +10,51 @@
 #include <mem/page.h>
 #include <vfs.h>
 
+void fs_test() {
+    status_t e;
+    if((e=vfs_mkdir_abs("/foo")) < 0) {
+        kprint("ERROR: Failed to create /foo: %d\n", (int)e);
+        return;
+    }
+    if((e=vfs_create_abs("/hello.txt")) < 0) {
+        kprint("ERROR: Failed to create /hello.txt: %d\n", (int)e);
+        return;
+    }
+}
+void ls(const char* path) {
+    status_t e;
+    Inode* dir;
+    if((e=vfs_open_abs(path, &dir)) < 0) {
+        kprint("Failed to open directory `%s`: %d\n", path, (int)e);
+        return;
+    }
+    off_t offset=0;
+    FsFuture future;
+    DirEntry entry;
+    char namebuf[128];
+    kprint("ls %s\n", path);
+    for(;;) {
+        if((e=inode_schedule_get_dir_entries(dir, &entry, offset, 1, &future)) < 0) {
+            if(e==-REACHED_EOF) break;
+            kprint("Failed to schedule get_dir_entries: %d\n", (int)e);
+            idrop(dir);
+            return;
+        }
+        while((e=future.complete(&future)) == (-PENDING));
+        if(e < 0) {
+            kprint("Failed to complete future: %d\n", (int)e);
+            idrop(dir);
+            return;
+        }
+        offset += e;
+        if((e=direntry_identify(&entry, namebuf, sizeof(namebuf))) < 0) {
+            kprint("Failed to identify: %d\n", (int)e);
+            continue;
+        }
+        kprint("   %s\n", namebuf);
+    }
+    idrop(dir);
+}
 void _start() {
     init_serial();
     init_framebuffer();
@@ -22,6 +67,9 @@ void _start() {
     kprint("Switched page tree.\n");
     init_vfs();
     init_scheduler();
+    fs_test();
+    ls("/");
+    ls("/foo");
     asm volatile("cli");
     for (;;) asm volatile("hlt");
 }
