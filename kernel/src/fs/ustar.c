5d9e90c3827501cfd09c16ea28aaa9548c43c05f
DcraftBg <czl_obshta@abv.bg>
2024-12-10 09:37:51 +0200

    [ustar] Error checking + writing to files

--- kernel/src/fs/ustar.c
+++ kernel/src/fs/ustar.c
@@ -1,5 +1,6 @@
 #include <fs/ustar.h>
 #include <kprint.h>
+#include <kpanic.h>
 #include <bootutils.h>
 #include <vfs.h>
 #include <string.h>
@@ -9,9 +10,25 @@
 #define FILESIZE_OFF  0x7C
 #define TYPE_OFF      156
 
+// TODO: Move this out to fsutils or some other module
+static status_t write_exact(Inode* file, const void* data, size_t size) {
+    status_t e;
+    off_t off=0;
+    while(size) {
+        if((e=inode_write(file, data, off, size)) < 0) return e;
+        if(e == 0) return -REACHED_EOF;
+        size -= e;
+        off  += e;
+        data += e;
+    }
+    return size;
+}
+
+// TODO: Boundary checks
 // TODO: Actually copy the file contents to the files
 
 void unpack_ustar() {
+    status_t e;
     char *at = (char*) boot_get_initrd();
     while (!memcmp(at + INDICATOR_OFF, "ustar", 5)) {
         size_t size = oct2bin(at + FILESIZE_OFF, 11);
@@ -23,11 +40,25 @@ void unpack_ustar() {
         kprint("%s found with name \"%s\", of size %zu bytes.\n", label, filename, size);
         if (is_dir) {
             filename[strlen(filename) - 1] = 0;
-            vfs_mkdir_abs(filename);
+            if((e=vfs_mkdir_abs(filename)) < 0)
+                kpanic("Failed to create directory `%s`: %d\n", filename, (int)e);
+            
             at += (((size + 511) / 512) + 1) * 512;
             continue;
         }
-        vfs_create_abs(filename);
+        {
+            if((e=vfs_create_abs(filename)) < 0)
+                kpanic("Failed to create file `%s`: %d", filename, (int)e);
+            
+            Inode* file;
+            if((e=vfs_open_abs(filename, &file)) < 0)
+                kpanic("Failed to open file `%s`: %d", filename, (int)e);
+            if((e=write_exact(file, at+512, size)) < 0) {
+                idrop(file);
+                kpanic("Failed to write to file `%s`: %d", filename, (int)e);
+            }
+            idrop(file);
+        }
         at += (((size + 511) / 512) + 1) * 512;
     }
     kprint("Unpack USTAR.\n");
